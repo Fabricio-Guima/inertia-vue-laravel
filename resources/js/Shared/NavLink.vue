28009b9e0a9eb31f44f8dc42a56b290d61a9cb62
Fabricio_Guima <fsgkof@gmail.com>
2025-04-19 13:50:50 -0300

    active links

---
+++ resources/js/Shared/NavLink.vue
@@ -0,0 +1,19 @@
+<template>
+    <Link
+    class="text-blue-500 hover:underline"
+    :class="{ 'font-bold underline': props.active }">
+    <slot />
+    </Link>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+const props = defineProps({
+    active: Boolean,
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
