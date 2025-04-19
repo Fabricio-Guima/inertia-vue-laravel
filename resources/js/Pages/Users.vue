705eff0833481c968cd5877de1eab8d744baf2c0
Fabricio_Guima <fsgkof@gmail.com>
2025-04-19 13:28:59 -0300

    example preserve the scroll position

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -4,11 +4,20 @@
         <h1 class="text-3xl">Users Page</h1>
     </div>
     <Nav />
+    <div style="margin-top: 800px;">
+        <p>The current time is {{ time }}.</p>
+
+        <Link href="/users" class="text-blue-500 hover:underline" preserve-scroll>Refresh</Link>
+    </div>
 </template>
 
 <script setup>
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 import Nav from '../Shared/Nav.vue';
+
+const props = defineProps({
+    time: String,
+});
 
 </script>
 
